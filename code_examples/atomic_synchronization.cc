b62199f152770eb1cad4f4dab7fa1a6c846386bf
Darío Suárez Gracia <dario@unizar.es>
2020-11-17 18:03:48 +0100

    Extra snippet examples

---
+++ code_examples/atomic_synchronization.cc
@@ -0,0 +1,32 @@
+#include <atomic>
+#include <iostream>
+#include <random>
+#include <thread>
+
+namespace {
+    std::atomic<int> shared_variable(0);
+}
+
+void increase_var(const size_t n)
+{
+    for(size_t i=0; i<n;i++) {
+        shared_variable++;
+    }
+}
+
+int main() {
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(512, 1024);
+    size_t n = dis(gen);
+
+    std::thread t(increase_var, n);
+
+    for(size_t i = 0; i < n; ++i) {
+        shared_variable--;
+    }
+    t.join();
+
+    std::cout << "shared_variable=" << shared_variable << std::endl;
+}
